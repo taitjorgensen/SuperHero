ab756d27252b8650ed9e12b595868c43ef81b5bc
taitjorgensen <taitjorgensen@gmail.com>
2018-11-29 10:57:33 -0600

    Initial push. Includes model, controller, and view for SuperHero. Also added bootstrap theme.

---
+++ SuperHeroes/SuperHeroes/Models/SuperHero.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SuperHeroes.Models
+{
+    public class SuperHero
+    {
+        [Key]
+        public int ID { get; set; }
+        public string SuperHeroName { get; set; }
+        public string AlterEgo { get; set; }
+        public string PrimaryAbility { get; set; }
+        public string SecondaryAbility { get; set; }
+        public string CatchPhrase { get; set; }
+
+    }
+}
